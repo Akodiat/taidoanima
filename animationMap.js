0891ed81b752c9edbffa3df0eb5892630a670401
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2022-07-01 09:51:46 +0200

    Add Jinsei and Seimei no hokei

--- animationMap.js
+++ animationMap.js
@@ -3,6 +3,8 @@ const animationMap = {
     "senin-hokei": ["resources/animations/senin-hokei.vmd"],
     "untai-hokei": ["resources/animations/untai-hokei.vmd"],
     "hentai-hokei": ["resources/animations/hentai-hokei.vmd"],
+    "seimei-hokei": ["resources/animations/seimei-hokei.vmd"],
+    "jinsei-hokei": ["resources/animations/jinsei-hokei.vmd"],
     "jodan-kobo": ["resources/animations/jodan-kobo-1.vmd", "resources/animations/jodan-kobo-2.vmd"],
     "chudan-kobo": ["resources/animations/chudan-kobo-1.vmd", "resources/animations/chudan-kobo-2.vmd"]
 }
